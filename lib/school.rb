537249534961f03bfe1281c1b26ad932378cbd15
kkirby16 <kkirby16@my.whitworth.edu>
2019-12-26 18:37:36 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -20,9 +20,9 @@ def grade(grade)
 end 
 
 def sort
-    #self.roster.each_value { |v| v.sort! }
+    self.roster.each_value { |v| v.sort! }
 
-    self.roster.each { |k,v| v.sort! }
+    #or can use self.roster.each { |k,v| v.sort! } or could use @roster instead of self.roster but self.roster is better to use. 
 end 
 
 end 
