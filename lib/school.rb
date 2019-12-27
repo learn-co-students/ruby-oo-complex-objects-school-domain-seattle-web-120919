46ddc27a1d24fd4340c9d9ea23c260721d027f54
kkirby16 <kkirby16@my.whitworth.edu>
2019-12-26 18:32:37 -0800

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,28 @@
 class School
 
+attr_reader :name, :roster
+
+def initialize(name)
+    @name = name #instance variable
+    @roster = {}
+end 
+
+def add_student(name, grade)
+    if @roster.has_key?(grade) 
+        @roster[grade] << name 
+    else 
+        @roster[grade] = [name]
+    end 
+end 
+
+def grade(grade)
+    @roster[grade]
+end 
+
+def sort
+    @roster.each_value { |v| v.sort! }
+
+    #or can use @roster.each { |k,v| v.sort! }
+end 
+
 end 
