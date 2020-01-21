11dce9023b949c4e2ed7816b648ab8b0978743fa
github username <github email address>
2020-01-21 13:32:24 -0800

    Redone

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,26 @@
 require "pry"
+
 class School
+  attr_reader :roster
+  attr_accessor :student
 
-    attr_accessor :roster
+  def initialize(roster)
+    @roster = {}
+  end
 
-    def initialize(roster)
-        @roster = {}
-    end
+  # how do we add multiple students to a class grade
+  def add_student(student, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student
+    # binding.pry
+  end
 
-    def add_student(name, grade)
-        @roster[grade] ||= []
-        @roster[grade] << name
-    end
+  def grade(year)
+    @roster[year]
+    # binding.pry
+  end
 
-    def grade(student_grade)
-        @roster[student_grade]
-    end
-
-    def sort
-        @roster.map {|k,v| [k,v.sort]}.to_h
-    end
-
+  def sort
+    @roster.each { |grade, students| students.sort! }
+  end
 end
